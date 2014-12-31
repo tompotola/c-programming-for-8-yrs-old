73ebed97f5c614b88ac5bb5d181d82c0856fd403
tompotola <jcvjetko@holisticware.com>
2014-12-31 15:05:02 +0100

    1st Tompotolas (Jana's) c program in MonoDevelop

---
+++ c-01/c-01/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	char word[80];
+	/*printf ("Kak se zoves??????");*/
+	printf("Have you got a dog? ");
+	fgets(word, sizeof(word), stdin); 
+	printf("Vau!Vau! \t%s\n",word);
+
+
+return 0;
+
+	}
