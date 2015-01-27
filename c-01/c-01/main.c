9d50565b1a297c689143b47f9831203f28b5e7b4
tompotola <jcvjetko@holisticware.com>
2015-01-27 20:10:32 +0100

    if statement + method/function/procedure

--- c-01/c-01/main.c
+++ c-01/c-01/main.c
@@ -1,14 +1,33 @@
 ﻿#include <stdio.h>
 
+void prvi_program();
+
 int main (int argc, char *argv[])
+{
+	prvi_program();
+
+	getchar();   
+
+	return 0;
+
+}
+
+void prvi_program()
 {
 	char word[80];
 	/*printf ("Kak se zoves??????");*/
 	printf("Have you got a dog? ");
 	fgets(word, sizeof(word), stdin); 
-	printf("Vau!Vau! \t%s\n",word);
 
 
-return 0;
-
+	if ( strcmp(word, "yes") == 0)
+	{
+		printf("Vau!Vau! \t%s\n",word);
 	}
+	else
+	{
+		printf("Miau!Miau! \t%s\n",word);
+	}
+
+	return;
+}
